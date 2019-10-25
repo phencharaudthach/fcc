83760f5747ead3c86a640ff2dc61b3bcea619136
Phen Thach <pshannonthach@gmail.com>
2019-10-24 20:16:54 -0400

    return true if ending matches the given target

---
+++ basicalgorithms/confirmtheending.js
@@ -0,0 +1,17 @@
+// Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+// This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+
+
+function confirmEnding(str, target) {
+    // "Never give up and good luck will find you."
+    // -- Falcor
+   if (str.substring(str.length-target.length) == target){
+       return true; 
+   } else {
+       return false;
+   }
+  }
+  
+  console.log(confirmEnding("Congratulation", "on"));
+  
